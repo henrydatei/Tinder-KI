f3ef98460512c54cefb2fa3dd33cd8745529ce4f
Henry Haustein <henrydatei@web.de>
2020-10-21 17:37:14 +0200

    some experimenting with features

--- tinderbot.py
+++ tinderbot.py
@@ -1,11 +1,61 @@
-from core.lib import TinderBot
-import configparser
-config = configparser.ConfigParser()
-config.sections()
+from core.lib import *
+from datetime import datetime, timedelta, timezone
+import pytz
+from dateutil.parser import parse
+from dateutil.relativedelta import relativedelta
+import json
 
-if __name__ == '__main__':
-    config.read('config.ini')
-    default_config = config['DEFAULT']
+def yearsago(years, from_date=None):
+    if from_date is None:
+        from_date = datetime.now()
+    return from_date - relativedelta(years=years)
 
-    bot = TinderBot(x_auth_token=default_config['Token'])
-    bot.random_like(is_message_starter=False)
+def getAge(person):
+    begin = parse(person.birth_date)
+    end = datetime.now(pytz.utc)
+    num_years = int((end - begin).days / 365.25)
+    if begin > yearsago(num_years, end):
+        return num_years - 1
+    else:
+        return num_years
+
+api = TinderAPI("token")
+
+#print(api.profile.user_id)
+
+# for match in api.matches():
+#     #print(match.person.name + " - " + str(getAge(match.person)))
+#     messages = api.getChat(match.match_id)
+#     print(match.match_id + " - " + str(len(messages)))
+
+match = api.matches()[20]
+# for match in matches:
+#     try:
+#         print(match.person.name + " - " + match.last_message[0].message)
+#     except:
+#         pass
+
+#print(str(len(matches)))
+#print(api.message("5f31b394c4eca70100733efb", "Warum hast du mich nicht zuerst angeschrieben?"))
+
+#messages = api.getChat("5f1e858e8702cc0100e13d9c5f7330dc3b3b1b01000235e4")
+#print(str(len(messages)))
+#for msg in messages:
+#    print(msg.message_from + ": " + msg.message)
+
+data = {}
+person = match.person
+data['name'] = person.name
+data['id'] = person.person_id
+data['birth_date'] = person.birth_date
+data['age'] = getAge(person)
+data['photo_urls'] = person.photo_urls
+data['match_id'] = match.match_id
+data['messages'] = []
+messages = api.getChat(match.match_id)
+for msg in messages:
+    msgObj = {'from': msg.message_from, 'to': msg.message_to, 'time': msg.timestamp, 'content': msg.message}
+    data['messages'].append(msgObj)
+
+with open('chat.json', 'w') as outfile:
+    json.dump(data, outfile)
